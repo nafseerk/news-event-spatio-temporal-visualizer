04e4143a91a909e675d1bcf660d470232a8c27b9
Nafseer Kadiyaravida <nafseerk@gmail.com>
2018-07-08 22:51:24 -0400

    1. dateRangeSlider bounds determined from the data 2. Refatoring changes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,11 @@
 $(document).ready(function(){
 
-//
-// Commons
-//
-
-var startDate,
-	endDate,
-	spatialPoints = [],
-	entities = {};
-
-function entity() {
-
-	// Setters
-	this.id = function(_) {
-      if (!arguments.length) return id;
-      id = _;
-      return this;
-    };
-
-    this.points = function(_) {
-      if (!arguments.length) return points;
-      points = _;
-      return this;
-    };
-
-}
-
-//
-// Helpers
-//
-
-var weekday = new Array(7);
-weekday[0]=  "Sun.";
-weekday[1] = "Mon.";
-weekday[2] = "Tue.";
-weekday[3] = "Wed.";
-weekday[4] = "Thu.";
-weekday[5] = "Fri.";
-weekday[6] = "Sat.";
+/*#################################################
+					Commons
+###################################################*/
 
 function formatDate(date) {
 	return d3.time.format("%Y-%m-%d %H:%M:%S").parse(date);
-}
-
-function print_filter(filter){
-	var f=eval(filter);
-	if (typeof(f.length) != "undefined") {}else{}
-	if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
-	if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
-	console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
 }
 
 function getColorFromScore(score) {
@@ -65,117 +22,146 @@ function getColorFromScore(score) {
 	}
 }
 
-//
-// Setup
-//
+function convertTimestampToUTCDate(timestampInSeconds) {
+	var targetTime = new Date(timestampInSeconds*1000);
+	return new Date(targetTime.getUTCFullYear(), targetTime.getUTCMonth(), targetTime.getUTCDate(),  targetTime.getUTCHours(), targetTime.getUTCMinutes(), targetTime.getUTCSeconds());
+}
 
-minDate = formatDate("2008-10-01 00:00:00");
-maxDate = formatDate("2008-10-28 23:59:59");
-startDate = formatDate("2008-10-16 21:15:00");
-endDate = formatDate("2008-10-23 21:15:00");
+/*#################################################
+					Map
+###################################################*/
+ 
 
-// Map parameters
 var mapZoomLevel = 2; // 1 = Full World view 
 var initialPosition = [0, 0]; // (0, 0) is center of earth 
 
+var map = L.map('map').setView(initialPosition, mapZoomLevel),
+		  maplink = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day.grey/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', 
+		  						{
+									attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+									subdomains: '1234',
+									mapID: 'newest',
+									app_id: 'DemoAppId01082013GAL',
+									app_code: 'AJKnXv84fjrb0KIHawS0Tg',
+									base: 'base',
+									minZoom: 0,
+									maxZoom: 18
+							    }).addTo(map);
 
-//
-// Slider
-//
+// Initialize the SVG layer
+map._initPathRoot();
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+/*#################################################
+					D3
+###################################################*/
 
-$("#slider").dateRangeSlider({
-	bounds: {min: minDate, max: maxDate},
-	defaultValues: {min: startDate, max: endDate},
-	wheelMode: "scroll",
-	wheelSpeed: 10,
-	step: {
-	    minutes: 5
-	},
-	formatter: function(val) {
-		var format = d3.time.format("%d %a. %H:%M");
-		return format(val);
-	},
-	scales: [{
-	  first: function(value) { return value; },
-	  end: function(value) {return value; },
-	  next: function(value) {
-	    var next = new Date(value);
-	    return new Date(next.setDate(value.getDate() + 1));
-	  },
-	  label: function(value){
-	  	var next = new Date(value);
-	    return next.getDate();
-	  },
-	  format: function(tickContainer, tickStart, tickEnd){
-	    tickContainer.addClass("myCustomClass");
-	  }
-	}, {
-	  first: function(value) { return value; },
-	  end: function(value) {return value; },
-	  next: function(value) {
-	    var next = new Date(value);
-	    return new Date(next.setHours(value.getHours() + 6));
-	  },
-	  label: function(value){
-	    return null;
-	  },
-	  format: function(tickContainer, tickStart, tickEnd){
-	    tickContainer.addClass("myCustomClass");
-	  }
-	}]
-});
+data_file_path = 'csv/2018-07-08_historic_parsed_scored.csv'
 
-d3.csv("csv/test_sentiment_scores.csv", function(collection) {
+d3.csv(data_file_path, function(collection) {
 
-	/*setTimeout(function () {
-		$('.inner').fadeTo('slow', 0.4);
-	}, 3000);
-
-	// when hover over text
-	$('.inner').hover(
-		function(){
-			$('.inner').stop().fadeTo('slow', 1);
-		},
-		function(){
-			$('.inner').stop().fadeTo('slow', 0.4);
-		}
-	);*/
-
-	spatialPoints = [];
-
-	function convertTimestampToUTCDate(timestampInSeconds) {
-		var targetTime = new Date(timestampInSeconds*1000);
-		return new Date(targetTime.getUTCFullYear(), targetTime.getUTCMonth(), targetTime.getUTCDate(),  targetTime.getUTCHours(), targetTime.getUTCMinutes(), targetTime.getUTCSeconds());
-	}
-
-	// Add a LatLng object to each item in the dataset
+	var spatialPoints = [];
+	var minDate = Infinity
+	var maxDate = -Infinity
 	collection.forEach(function(d) {
+
+		if(d.timestamp < minDate){
+			minDate = d.timestamp
+		}
+		if(d.timestamp > maxDate){
+			maxDate = d.timestamp
+		}
 		spatialPoints.push({
+			tweet_id: parseInt(d.tweet_id),
 			coordinates: new L.LatLng(d.lat, d.lng),
 			date: convertTimestampToUTCDate(d.timestamp),
-			tweet_id: parseInt(d.tweet_id),
 			score: parseInt(d.score)
 		});
+	});
+
+	var startDate = minDate;
+	var endDate = (parseInt(minDate) + parseInt((maxDate - minDate) / 10)).toString();
+	
+	// Convert dates from timestamp to UTC
+	minDate = convertTimestampToUTCDate(minDate)
+	maxDate = convertTimestampToUTCDate(maxDate)
+	startDate = convertTimestampToUTCDate(startDate)
+	endDate = convertTimestampToUTCDate(endDate)
+	
+	$("#slider").dateRangeSlider({
+		bounds: {min: minDate, max: maxDate},
+		defaultValues: {min: startDate, max: endDate},
+		wheelMode: "scroll",
+		wheelSpeed: 10,
+		step: {
+			minutes: 5
+		},
+		formatter: function(val) {
+			var format = d3.time.format("%d %a. %H:%M");
+			return format(val);
+		},
+		scales: [{
+					first: function(value) { return value; },
+		  			end: function(value) {return value; },
+		  			next: function(value) {
+		    		var next = new Date(value);
+		    		return new Date(next.setDate(value.getDate() + 1));
+		  		 	},
+		  			label: function(value){
+		  			var next = new Date(value);
+		    		return next.getDate();
+		  		    },
+		  			format: function(tickContainer, tickStart, tickEnd){
+		    		tickContainer.addClass("myCustomClass");
+		  			}
+		         }, 
+				 {
+		    		first: function(value) { return value; },
+		  			end: function(value) {return value; },
+		  			next: function(value) {
+		    		var next = new Date(value);
+		    		return new Date(next.setHours(value.getHours() + 6));
+		  			},
+		  			label: function(value){
+		    		return null;
+		  			},
+		  			format: function(tickContainer, tickStart, tickEnd){
+		    		tickContainer.addClass("myCustomClass");
+		  			}
+				}]
 	});
 
 	$("#slider").bind("valuesChanging", function(e, data){
 		updatePointsWithRange([data.values.min, data.values.max]);
 	});
 
-	//
 	// CROSSFILTER
-	//
-
 	var spatial = crossfilter(spatialPoints),
 		all = spatial.groupAll(),
 		dateDimension = spatial.dimension(function (d) { return d.date; }),
-		tweetsDimension = spatial.dimension(function(d) { return d.tweet_id; }),
-		tweets = [];
+		tweetsDimension = spatial.dimension(function(d) { return d.tweet_id; });
+	
+	var tweets = [];
+	tweetsDimension.top(Infinity).forEach(function (d) {
+		tweets.push({tweet_id: d.tweet_id, score: d.score});
+	});
 
-	getAllTweets();
+	// Pick up the SVG from the map object
+	var svg = d3.select("#map").select("svg");
+	var mapPoints = svg.append("g");
 
+	// Use Leaflet to implement a D3 geometric transformation.
+	function projectPoint(x, y) {
+		var point = map.latLngToLayerPoint(new L.LatLng(x, y));
+		this.stream.point(point.x, point.y);
+	}
+
+	var transform = d3.geo.transform({point: projectPoint});
+	var	path = d3.geo.path().projection(transform);
+
+
+	// Points
+	
+	var entities = {};
 	function filterSpatialPointsWithRange(range) {
 		entities = {};
 		dateDimension.filterRange(range);
@@ -188,63 +174,11 @@ d3.csv("csv/test_sentiment_scores.csv", function(collection) {
 			entities[d.tweet_id].push(d.coordinates);
 		});
 		
-		/*
-		$("#countUsers").html(Object.keys(entities).length);
-		var timeDiff = range[1]-range[0];
-		$("#countTime").html(Math.floor(timeDiff / 1000 / 60 / 60));
-		*/
 		$('.inner').stop().fadeTo('slow', 1);
 		/*setTimeout(function() {
 			$('.inner').fadeTo('slow', 0.4);
 		}, 1000);*/
 	}
-
-	function getAllTweets() {
-		tweetsDimension.top(Infinity).forEach(function (d) {
-			tweets.push({tweet_id: d.tweet_id, score: d.score});
-		});
-	}
-
-	// Count total number of points
-	var n = all.reduceCount().value();
-	console.log("There are " + n + " points in total.");
-
-	//
-	// MAP
-	//
-
-	var map = L.map('map').setView(initialPosition, mapZoomLevel),
-		maplink = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.day.grey/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
-					attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
-					subdomains: '1234',
-					mapID: 'newest',
-					app_id: 'DemoAppId01082013GAL',
-					app_code: 'AJKnXv84fjrb0KIHawS0Tg',
-					base: 'base',
-					minZoom: 0,
-					maxZoom: 18
-				}).addTo(map);
-
-	// Initialize the SVG layer */
-	map._initPathRoot();
-
-	// Pick up the SVG from the map object */
-	var svg = d3.select("#map").select("svg"),
-		mapPoints = svg.append("g");
-
-	// Use Leaflet to implement a D3 geometric transformation.
-	function projectPoint(x, y) {
-		var point = map.latLngToLayerPoint(new L.LatLng(x, y));
-		this.stream.point(point.x, point.y);
-	}
-
-	var transform = d3.geo.transform({point: projectPoint}),
-		path = d3.geo.path().projection(transform);
-
-	//
-	// Points
-	//
-
 
 	filterSpatialPointsWithRange([startDate, endDate]);
 
@@ -300,5 +234,6 @@ d3.csv("csv/test_sentiment_scores.csv", function(collection) {
 	$(".description").show();
 
 });
+
 
 });
